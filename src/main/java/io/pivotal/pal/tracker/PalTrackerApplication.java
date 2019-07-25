cb9887fb6e52332c618c8c5fc46866dc4f5f22e8
shamesun <aim.sskushwaha@gmail.com>
2019-07-25 11:03:35 +0530

    jdbc code submitted

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,16 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    @Autowired
+    DataSource dataSource;
     @Bean
     public TimeEntryRepository getTimeEntryRepository()
     {
-        return  new InMemoryTimeEntryRepository();
+        return  new JdbcTimeEntryRepository(dataSource);
     }
 
     public static void main(String[] args) {
